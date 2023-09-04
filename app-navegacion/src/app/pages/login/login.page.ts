f5734a2a85950a5d1d823d64c4ae20ae5a31fead
= <saara.munoz88@gmail.com>
2023-09-04 09:59:35 -0300

    Back listo, pero falta front

--- app-navegacion/src/app/pages/login/login.page.ts
+++ app-navegacion/src/app/pages/login/login.page.ts
@@ -37,10 +37,14 @@ export class LoginPage implements OnInit {
     var passSinString = JSON.parse(passwordString ?? 'null');
     var usuario = usuarioString ? JSON.parse(usuarioString) : null;
 
-
-    if(usuario.nombre == f.nombre && passSinString == f.password){
-        this.navCtrl.navigateForward('/principal');
+    if (usuario.nombre == f.nombre && (f.password == usuario.password || f.password == passSinString)) //(passSinString === f.password ||
+    {
+      this.navCtrl.navigateForward('/principal');
     }else{
+      console.log(passSinString);
+      console.log(f.password);
+      console.log(localStorage);
+      console.log(usuario.password);
       const alert = await this.alertController.create({
         header: 'Datos incorrectos',
         message: 'Los datos que ingresaste son incorrectos.',
@@ -52,74 +56,3 @@ export class LoginPage implements OnInit {
   }
 
 }
-
-/*import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
-})
-export class LoginPage implements OnInit {
-
-  formularioLogin: FormGroup;
-
-  //mdl_usuario: string = ''
-  //mdl_pass: string = ''
-
-  //isAlertOpen = false;
-  //alertButtons = ['OK'];
-
-  constructor(public fb: FormBuilder,
-    public alertController: AlertController) {
-    this.formularioLogin = this.fb.group({
-      'nombre': new FormControl("",Validators.required),
-      'password': new FormControl("",Validators.required)
-    })
-  } 
-
-  ngOnInit() {
-  }
-
-  async ingresar(){
-    var f = this.formularioLogin.value;
-
-    var usuarioString = localStorage.getItem('usuario');
-    var usuario = usuarioString ? JSON.parse(usuarioString) : null;
-
-
-    if(usuario.nombre == f.nombre && usuario.password == f.password){
-      console.log('Ingresado');
-    }else{
-      const alert = await this.alertController.create({
-        header: 'Datos incorrectos',
-        message: 'Los datos que ingresaste son incorrectos.',
-        buttons: ['Aceptar']
-      });
-  
-      await alert.present();
-    }
-  }
-
- /* navegar() {
-    if(this.mdl_usuario == 'Christian' && this.mdl_pass == 'costra14'){
-      let parametros: NavigationExtras={
-        state: {
-          user: this.mdl_usuario,
-          pass: this.mdl_pass
-        }
-      }
-      this.router.navigate(['principal'], parametros);
-    } else {
-      this.isAlertOpen = true;
-    }
-  }
-
-  setOpen(isOpen: boolean) {
-    this.isAlertOpen = isOpen;
-  }
-}
-*/
